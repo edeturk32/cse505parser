891cc58b4a40de14b031f39a61975f1fc44bb997
duck-bongos <billmannd@gmail.com>
2021-11-29 15:09:04 -0500

    added more constraint parsing things

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-from lark import Lark, Transformer, Tree
+from lark import Lark, Transformer, Token, Tree
 
 
 class ProcessFlatZinc(Transformer):
@@ -66,6 +66,26 @@ def process_constraint_item(item):
             print(child.data)
 
 
+class Node:
+    def __init__(self, tree):
+        self.identifier = self._get_identifier(tree)
+        self.expressions = self._get_data(tree)
+        self.annotations = self._get_annotations(tree)
+
+    def _get_identifier(self, t: Tree):
+        return t.data
+
+    def _get_data(self, t: Tree):
+        expressions = []
+
+        y = [child for child in t.children if child.data.type == "expr"]
+        return y
+
+    def _get_annotations(self, t: Tree):
+        x = [child for child in t.children if child.data.type == "annotations"]
+        return x
+
+
 class VarDeclNode:
     def __init__(self, tree: Tree):
         self.data_type, self.data_value = self._get_data(tree)
@@ -101,21 +121,42 @@ class VarDeclNode:
         return False
 
 
+class ArrayNode(VarDeclNode):
+    def __init__(self, t: Tree):
+        self.data_type, self.data_value = self._get_data(t)
+        self.name_type, self.var_name = self._get_name(t)
+        self.var_type, _ = self._get_var_info(t)
+        self.var_values = self._get_arguments(t)
+        self.annotations = self._get_annotations(t)
+
+    def _get_arguments(self, t: Tree):
+        _args = []
+        for x in t.children:
+            if x.data.value == "array_literal":
+                for y in x.children:
+                    if y.data.value == "var_par_identifier":
+                        _args.append(y.children[0].value)
+        return _args
+
+
+def get_expression(tre: Tree):
+    expr = [c.children[0] for c in tre.children if c.data.value == "expr"]
+    ex = []
+    for e in expr.children:
+        if isinstance(e, Tree):
+            if e.data.value == "var_par_identifier":
+                ex.append(e.children[0].value)
+            else:
+                ex.append(e.children[0])
+    return ex
+
+
 class ConstraintNode(VarDeclNode):
     def __init__(self, tree):
         self.predicate = self._get_predicate(tree)
         self.arguments = self._get_arguments(tree)
-        self.value = self._get_value(tree)
-
-        """
-        self.data_type, self.data_value = self._get_data(tree)
-        self.name_type, self.var_name = self._get_name(tree)
-        self.var_type, self.var_values = self._get_var_info(tree)
         self.annotations = self._get_annotations(tree)
-        self.is_introduced = self._is_introduced()
-        self.children = tree.children
-        assert len(self.children) == 4
-        """
+        self.value = self._get_value(tree)
 
     def _get_value(self, tree):
         return tree.children[2].children
@@ -127,24 +168,33 @@ class ConstraintNode(VarDeclNode):
         return None
 
     def _get_arguments(self, tree):
-        for child in tree.children:
-            if (
-                child.data.value == "expr"
-                and child.children[0].data.value == "array_literal"
-            ):
-                return self._parse_arguments(child.children[0].children)
+        if self.predicate == "array_bool_or":
+            for child in tree.children:
+                if (
+                    child.data.value == "expr"
+                    and child.children[0].data.value == "array_literal"
+                ):
+                    return self._parse_arguments(child.children[0].children)
 
-    def _get_arguments(self, tree):
-        x = []
-        for child in tree.children:
-            if child.data == "expr":
-                x = [
-                    node.children[0].value
-                    for node in child.children
-                    if isinstance(node, Tree)
+        elif self.predicate == "int_lin_eq":
+            for child in tree.children:
+                if child.data == "expr":
+                    return child.children[0].children
+
+        elif self.predicate == "bool2int":
+            return [
+                x[0].value
+                for x in [
+                    tree.children[i].children[0].children
+                    for i in range(1, len(tree.children))
+                    if tree.children[i].data == "expr"
                 ]
-                return x
-        return
+            ]
+
+        elif self.predicate == "int_lin_le":
+            return get_expression(tree)
+        else:
+            print(self.predicate)
 
     def _parse_arguments(self, tree_args):
         if all([isinstance(item, Tree) for item in tree_args]):
@@ -154,35 +204,41 @@ class ConstraintNode(VarDeclNode):
 
 
 if __name__ in "__main__":
-    flatzinc = open("vertex_cover.fzn", "r")
+    # flatzinc = open("vertex_cover.fzn", "r")
+    flatzinc = open("qk.fzn", "r")
     tree = ProcessFlatZinc().transform(Lark.open("grammar.lark").parse(flatzinc.read()))
     obj_func = []
     introduced_nodes = []
     constraints = []
+    array_nodes = []
     for item in tree.children:
         if item == None:
             continue
         else:
             if item.data.value == "var_decl_item":
-                n = VarDeclNode(item)
+                if item.children[0].data == "array_var_type":
+                    an = ArrayNode(item)
+                    array_nodes.append(an)
 
-                if n.var_name == "v":
-                    print()
-                if n.is_introduced:
-                    introduced_nodes.append(n)
                 else:
-                    obj_func.append(n)
+                    n = VarDeclNode(item)
+
+                    if n.is_introduced:
+                        introduced_nodes.append(n)
+                    else:
+                        obj_func.append(n)
 
             elif item.data.value == "constraint_item":
                 cn = ConstraintNode(item)
                 if cn.predicate == "bool2int":
                     print()
-                constraints.append(ConstraintNode(item))
+                constraints.append(cn)
 
             else:
                 pass
 
-    print(len(obj_func), len(introduced_nodes), len(constraints))
+    # print(len(obj_func), len(introduced_nodes), len(constraints), len(array_nodes))
+    print(f"\n\n{'-'*100}\n\n")
     for of in obj_func:
         print(of.var_name, of.var_values)
 
@@ -193,3 +249,9 @@ if __name__ in "__main__":
     print(f"\n\n{'-'*100}\n\n")
     for c in constraints:
         print(c.predicate, c.arguments)
+
+    print(f"\n\n{'-'*100}\n\n")
+    for a in array_nodes:
+        print(a.var_type, a.var_values)
+
+    print(f"\n\n{'-'*100}\n\n")
