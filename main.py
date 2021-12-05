e20dd498dac326b600a69fcdf70d7ba001897a8a
Ethan DeTurk <ethan.deturk@stonybrook.edu>
2021-12-05 14:31:21 -0500

    Print parser output

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from parse.parse import parse_flatzinc
 if __name__ in "__main__":
     flatzinc = open(sys.argv[1], "r")
     decision_vars, A_le, b_le, A_eq, b_eq, objective = parse_flatzinc(flatzinc)
+    print("Decision variables (identifier: range):")
+    for identifier, rng in decision_vars:
+        print("{}: {}".format(identifier, rng))
+    print()
+    print("Objective function to minimize: ", objective)
